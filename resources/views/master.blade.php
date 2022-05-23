edef3ccf9eaa74404aef9e3e41e00a783d63b22a
timubin <mubin3242@gmail.com>
2022-05-23 07:24:57 -0700

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include("includes.meta")
+    <title>@yield('title')</title>
+   @include('includes.css')
+
+</head><!--/head-->
+
+<body>
+	@include("includes.header")
+	
+    @yield('body')
+	
+    @include('includes.footer');
+	
+
+
+    @include('includes.js');
+   
+</body>
+</html>
